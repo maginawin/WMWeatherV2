8b9b56e1d0539d5565d12d9cbdbfb600c48a4fc1
wangwendong <wangwendong@wangwendongs-MacBook-Pro.local>
2015-08-29 13:15:48 +0800

    Initial Commit

---
+++ WMWeatherV2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WMWeatherV2
+//
+//  Created by wangwendong on 15/8/29.
+//  Copyright (c) 2015年 wangwendong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WMWeatherV2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WMWeatherV2
+//
+//  Created by wangwendong on 15/8/29.
+//  Copyright (c) 2015年 wangwendong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
